26f3f19f4067bec93ff878373586c5b24d2ff632
mbanixox <46408125+mbanixox@users.noreply.github.com>
2025-09-19 20:43:09 +0300

    Add Games context for RAWG API operations
    -Create Games context module for game-related operations

---
+++ lib/swapnplay/games.ex
@@ -0,0 +1,56 @@
+defmodule Swapnplay.Games do
+  @moduledoc """
+  Context for game-related operations using RAWG API
+  """
+
+  alias Swapnplay.RawgClient
+
+  def fetch_genres do
+    RawgClient.get("/genres")
+  end
+
+  def fetch_games(options \\ []) do
+    search_query = Keyword.get(options, :search)
+    platforms = Keyword.get(options, :platforms, RawgClient.default_platforms())
+    page = Keyword.get(options, :page, 1)
+    page_size = Keyword.get(options, :page_size, 20)
+
+    params = %{
+      platforms: platforms,
+      page: page,
+      page_size: page_size,
+    }
+
+    params = if search_query, do: Map.put(params, :search, search_query), else: params
+
+    RawgClient.get("/games", params)
+  end
+
+  def fetch_games_by_genre(genre_query, options \\ []) do
+    platforms = Keyword.get(options, :platforms, RawgClient.default_platforms())
+    page = Keyword.get(options, :page, 1)
+    page_size = Keyword.get(options, :page_size, 20)
+
+    params = %{
+      platforms: platforms,
+      genre: genre_query,
+      page: page,
+      page_size: page_size,
+    }
+
+    RawgClient.get("/games", params)
+  end
+
+  def fetch_genre_details(genre_query) do
+    RawgClient.get("/genres/#{genre_query}")
+  end
+
+  def fetch_game_details(game_id) do
+    RawgClient.get("/games/#{game_id}")
+  end
+
+  def fetch_game_screenshots(game_id) do
+    RawgClient.get("/games/#{game_id}/screenshots")
+  end
+
+end
