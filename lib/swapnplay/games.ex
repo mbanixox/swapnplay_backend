8e480a2c6091cf8e7f581115b5210874c46de11e
mbanixox <46408125+mbanixox@users.noreply.github.com>
2025-09-26 01:23:08 +0300

    Update Games context with caching

--- lib/swapnplay/games.ex
+++ lib/swapnplay/games.ex
@@ -3,18 +3,28 @@ defmodule Swapnplay.Games do
   Context for game-related operations using RAWG API
   """
 
-  alias Swapnplay.RawgClient
+  alias Swapnplay.{RawgClient, CacheService}
 
   def fetch_genres do
-    case RawgClient.get("/genres") do
-      {:ok, response} ->
-        filtered_response =
-          response
-          |> Map.delete("next")
-          |> Map.delete("previous")
-        {:ok, filtered_response}
-      {:error, reason} -> {:error, reason}
-    end
+    CacheService.fetch_with_cache(
+      :genres_cache,
+      "all_genres",
+      :genres,
+      fn ->
+        case RawgClient.get("/genres") do
+          {:ok, response} ->
+            filtered_response =
+              response
+              |> Map.delete("next")
+              |> Map.delete("previous")
+
+            {:ok, filtered_response}
+
+          {:error, reason} ->
+            {:error, reason}
+        end
+      end
+    )
   end
 
   def fetch_games(options \\ []) do
@@ -33,34 +43,106 @@ defmodule Swapnplay.Games do
     params = if search_query, do: Keyword.put(params, :search, search_query), else: params
     params = if genre_query, do: Keyword.put(params, :genres, genre_query), else: params
 
-    case RawgClient.get("/games", params) do
-      {:ok, response} ->
-        filtered_response =
-          response
-          |> Map.delete("next")
-          |> Map.delete("previous")
-        {:ok, filtered_response}
-      {:error, reason} -> {:error, reason}
-    end
+    # Generate a cache key based on the parameters
+    cache_key = CacheService.cache_key("games", params)
+
+    cache_type =
+      cond do
+        search_query -> :games_search
+        genre_query -> :games_by_genre
+        true -> :games_list
+      end
+
+    CacheService.fetch_with_cache(
+      :games_cache,
+      cache_key,
+      cache_type,
+      fn ->
+        case RawgClient.get("/games", params) do
+          {:ok, response} ->
+            filtered_response =
+              response
+              |> Map.delete("next")
+              |> Map.delete("previous")
+
+            {:ok, filtered_response}
+
+          {:error, reason} ->
+            {:error, reason}
+        end
+      end
+    )
   end
 
   def fetch_genre_details(genre_query) do
-    RawgClient.get("/genres/#{genre_query}")
+    cache_key = "genre_details:#{genre_query}"
+
+    CacheService.fetch_with_cache(
+      :genres_cache,
+      cache_key,
+      :genre_details,
+      fn -> RawgClient.get("/genres/#{genre_query}") end
+    )
   end
 
   def fetch_game_details(game_id) do
-    RawgClient.get("/games/#{game_id}")
+    cache_key = "game_details:#{game_id}"
+
+    CacheService.fetch_with_cache(
+      :games_cache,
+      cache_key,
+      :game_details,
+      fn -> RawgClient.get("/games/#{game_id}") end
+    )
   end
 
   def fetch_game_screenshots(game_id) do
-    case RawgClient.get("/games/#{game_id}/screenshots") do
-      {:ok, response} ->
-        filtered_response =
-          response
-          |> Map.delete("next")
-          |> Map.delete("previous")
-        {:ok, filtered_response}
-      {:error, reason} -> {:error, reason}
+    cache_key = "game_screenshots:#{game_id}"
+
+    CacheService.fetch_with_cache(
+      :games_cache,
+      cache_key,
+      :game_screenshots,
+      fn ->
+        case RawgClient.get("/games/#{game_id}/screenshots") do
+          {:ok, response} ->
+            filtered_response =
+              response
+              |> Map.delete("next")
+              |> Map.delete("previous")
+
+            {:ok, filtered_response}
+
+          {:error, reason} ->
+            {:error, reason}
+        end
+      end
+    )
+  end
+
+  # Cache management functions
+
+  def invalidate_game_cache(game_id) do
+    patterns = [
+      "game_details:#{game_id}",
+      "game_screenshots:#{game_id}"
+    ]
+
+    Enum.each(patterns, &CacheService.invalidate_cache(:games_cache, &1))
+  end
+
+  def invalidate_genres_cache(genre_query \\ nil) do
+    if genre_query do
+      CacheService.invalidate_cache(:genres_cache, "genre_details:#{genre_query}")
+    else
+      CacheService.clear_cache(:genres_cache)
     end
+  end
+
+  def get_cache_stats do
+    %{
+      games_cache: CacheService.get_cache_stats(:games_cache),
+      genres_cache: CacheService.get_cache_stats(:genres_cache)
+    }
   end
 end
